6aaba26942c1ecec8960e4ba836bde75aa20dcb7
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-04-25 15:19:43 +0300

    click

--- js-drum-kit/js/index.js
+++ js-drum-kit/js/index.js
@@ -1,20 +1,36 @@
 'use strict';
 
-const removeTransition = e => {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
-};
+(function() {
+  const removeTransition = e => {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  };
 
-const playSound = e => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const onKeyDown = e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    playSound(key, audio);
+  };
 
-  if (!audio) return;
-  key.classList.add('playing');
-  audio.currentTime = 0;
-  audio.play();
-};
+  const onMouseDown = e => {
+    const key = e.target;
+    const audio = document.querySelector(
+      'audio[data-key="' + e.target.getAttribute('data-key') + '"]'
+    );
+    playSound(key, audio);
+  };
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+  const playSound = (key, audio) => {
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', onMouseDown);
+  });
+  window.addEventListener('keydown', onKeyDown);
+})();
